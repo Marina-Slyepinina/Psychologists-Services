acef58845ba309a26f1a0ba6ab32ee0b6641543d
Marina <marina.slepinina3@gmail.com>
2025-11-15 17:01:35 +0200

    added basic header markup

---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import css from "./Container.module.css";
+
+type ContainerProps = {
+    children: React.ReactNode
+}
+
+export const Container = ({children}: ContainerProps) => {
+  return (
+      <div className={css.container}>{children}</div>
+  )
+}
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,26 +1,39 @@
-import { Link } from "react-router"
+import { Link, NavLink } from "react-router"
 import { Button } from "../Button/Button"
 
 import css from "./Header.module.css";
+import { Container } from "../Container/Container";
 
 const handleClick = () => {
 }
 
 export const Header = () => {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li><Link to="/"><span className="">psychologists.</span>services</Link></li>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/psychologists">Psychologists</Link></li>
-          {/* <li><Link to="/favorites">Favorites</Link></li> */}
+    <header className={css.header}>
+      <Container>
+        <div className={css.headerContainer}>
+          <nav className={css.navigation}>
+            <ul className={css.navList}>
+              <li className={css.logo}>
+                <Link to="/"><span className={css.accent}>psychologists.</span >services</Link>
+              </li>
+              <li className={css.navItemContainer}>
+                <NavLink to="/" className={({ isActive }) => isActive ? css.active : ""}>Home</NavLink>
+              </li>
+              <li className={css.navItemContainer}>
+                <NavLink to="/psychologists" className={({ isActive }) => isActive ? css.active : ""}>Psychologists</NavLink>
+              </li>
+              <li className={css.navItemContainer}>
+                <NavLink to="/favorites" className={({ isActive }) => isActive ? css.active : ""}>Favorites</NavLink>
+              </li>
+            </ul>
+          </nav>
           <div className={css.btnContainer}>
             <Button handleClick={handleClick} variant="transparent" text="Log In" horizontalPaddings={39} />
             <Button handleClick={handleClick} variant="filled" text="Registration" horizontalPaddings={40}/>
           </div>
-        </ul>
-      </nav>
+        </div>
+      </Container>
     </header>
   )
 }

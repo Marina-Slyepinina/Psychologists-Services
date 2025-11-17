475a8b7bc904541e4e842f1d68120777eb3b55e3
Marina <marina.slepinina3@gmail.com>
2025-11-17 10:48:24 +0200

    added Modal

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,53 @@
+import React, { useEffect } from 'react'
+import css from "./Modal.module.css";
+import { createPortal } from 'react-dom';
+
+type ModalProps = {
+    children: React.ReactNode,
+    onClose: () => void,
+    title?: string,
+    mainText?: string,
+    contentWidth?: number
+}
+
+
+export const Modal = ({ children, onClose, title, mainText, contentWidth }: ModalProps) => {
+
+    const handleBackdrop = (e: React.MouseEvent<HTMLDivElement>) => {
+        if (e.target === e.currentTarget) {
+            onClose();
+        }
+    }
+    
+    useEffect(() => {
+        const onEscape = (e: KeyboardEvent) => {
+            if (e.code === "Escape") {
+                onClose();
+            }
+        };
+        document.addEventListener("keydown", onEscape);
+        const originalOverflow = document.body.style.overflow;
+        document.body.style.overflow = "hidden";
+
+        return () => {
+            document.removeEventListener("keydown", onEscape);
+            document.body.style.overflow = originalOverflow;
+        };
+    }, [onClose]);
+    
+    return createPortal(
+        <div className={css.backdrop} onClick={handleBackdrop}>
+            <div className={css.modalWrapper}>
+                <div className={css.contentWrapper} style={{ width: contentWidth }}>
+                    <svg width={32} height={32} className={css.iconClose} onClick={onClose}>
+                        <use href="sprite.svg#close"></use>
+                    </svg>
+                    <h2 className={css.modalTitle}>{title}</h2>
+                    <p className={css.modalText}>{mainText}</p>
+                </div>
+                {children}
+            </div>
+        </div>,
+        document.body
+    )
+}

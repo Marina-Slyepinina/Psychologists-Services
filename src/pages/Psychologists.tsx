25323cba5374cb47d3bb0fa2137ba437d7f59580
Marina <marina.slepinina3@gmail.com>
2025-11-30 20:11:07 +0200

    added Psychologists Page

--- src/pages/Psychologists.tsx
+++ src/pages/Psychologists.tsx
@@ -1,7 +1,62 @@
+import { useEffect } from "react";
+import { usePsychologistsStore } from "../store/psychologistsStore";
+import { Container } from "../components/Container/Container";
+import { FilterDropdown } from "../components/FilterDropdown/FilterDropdown";
+import { Card } from "../components/Card/Card";
+import { Button } from "../components/Button/Button";
+import css from "./Psychologists.module.css";
+
 export const Psychologists = () => {
+
+  const {
+    psychologists,
+    isLoading,
+    hasMore,
+    loadInitialData,
+    loadMore
+  } = usePsychologistsStore();
+
+  const currentSettings = usePsychologistsStore(state => state.currentSettings);
+
+  useEffect(() => {
+    if (psychologists.length === 0 && !isLoading) {
+      loadInitialData();
+    }
+  }, [currentSettings, loadInitialData, isLoading, psychologists.length]);
+
+
+  const showLoadMoreButton = hasMore && !isLoading && psychologists.length > 0;
+  const showLoadingOnInitial = isLoading && psychologists.length === 0;
+
   return (
-    <>
-      <div>Psychologists</div>
-    </>
+    <section className={css.psychologistsPage}>
+      <Container className={css.container}>
+
+        <div className={css.filterDropdown}>
+          <FilterDropdown />
+        </div>
+
+        {!showLoadingOnInitial && psychologists.length > 0 && (
+          <div className={css.psychologistList}>
+            {psychologists.map((psychologist) => (
+              <Card key={psychologist.id} psychologist={psychologist} />
+            ))}
+          </div>
+        )}
+
+        {showLoadMoreButton && (
+          <div className={css.btnWrapper}>
+            <Button
+              type="button"
+              variant="filled"
+              handleClick={loadMore}
+            >
+              Load more
+            </Button>
+          </div>
+        )}
+
+      </Container>
+    </section>
   );
 };
